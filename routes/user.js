8eb755bb9d6cb528b723679ff5a0d21cda2274c3
Diego Veas <diego81@hotmail.cl>
2023-03-27 02:31:00 -0300

    Primer commit

---
+++ routes/user.js
@@ -0,0 +1,14 @@
+
+const {Router} = require('express');
+const { usuariosGet, usuariosDelete, usuariosPut, usuariosPost, usuariosPatch } = require('../controllers/usuarios');
+
+const router = Router();
+
+router.get('/', usuariosGet );
+router.put('/:id', usuariosPut );
+router.post('/', usuariosPost );
+router.delete('/', usuariosDelete );
+router.patch('/', usuariosPatch);
+
+
+module.exports = router;
